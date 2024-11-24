91adf314afa8ad2b5baee5920cfc37e1aa0987a7
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-24 23:06:39 +0700

    add: intro screen

---
+++ app/navigators/AppNavigator/AppNavigator.js
@@ -0,0 +1,29 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+// import Toast from "react-native-toast-message";
+
+import Intro from "../../screens/intro/Intro";
+import MainTabNavigator from "../MainTabNavigator/MainTabNavigator";
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Intro"
+                screenOptions={{
+                    headerShown: false,
+                    gestureEnabled: false,
+                }}
+            >
+                <Stack.Screen name="Intro" component={Intro} />
+                <Stack.Screen name="Home" component={MainTabNavigator} />
+            </Stack.Navigator>
+            {/* <Toast /> */}
+        </NavigationContainer>
+    );
+};
+
+export default AppNavigator;
---
+++ app/screens/intro/Intro.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+// import { introspect } from "../../services/authService";
+// import { getToken, deleteToken } from "../../utils/authStorage";
+
+import logo from "../../assets/img/logo.png";
+
+const Intro = ({ navigation }) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.replace("Home", {
+                screen: "HomeTab",
+            });
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+
+            <Image source={logo} style={styles.image} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    image: {
+        width: 120,
+        height: 120,
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+});
+
+export default Intro;
